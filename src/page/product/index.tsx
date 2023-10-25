27d5400679258931db4ef706c268418128e68384
gabriel <gabrildado08@gmail.com>
2023-10-25 11:26:05 -0300

    product page reponsive

--- src/page/product/index.tsx
+++ src/page/product/index.tsx
@@ -40,30 +40,29 @@ export function Product() {
     return (
         <>
             
-            <main className="">
+            <main className="w-100 flex justify-center">
                 {product.map((item) =>(
-                <div className="container gap-3  mx-auto h-96 max-w-[90%] mt-[150px] p-10 grid grid-flow-row-dense grid-cols-2 gap-4 mb-10  ">
-
-                    <div className='rounded col-span-1  flex gap-5 w-[120%] mx-auto shadow-2xl bg-white'>
+                <div className="container gap-3  xl:mx-auo h-100 max-w-[90%] mt-[150px] flex max-xl:flex-wrap p-10 mb-10">
+                    <div className='rounded justify-center h-auto flex gap-2 flex-wrap w-[100%] mx-auto shadow-2xl bg-white'>
                         <div>
-                            <img className='object-contain aspect-square  max-w-xs  ' src={item.url} alt="" />
+                            <img className='object-contain aspect-square  max-w-sm w-80  ' src={item.url} alt="" />
                         </div>
-                        <div>
-                            <h1 className='relative top-10 text-1xl font-bold'>
+                        <div className='flex flex-col gap-8 mt-10 p-10 mb-10'>
+                            <h1 className='relativ top-10 text-1xl font-bold'>
                                 {item.ProductName}
                             </h1>
-                            <p className='relative top-20 '>
+                            <p className='sm:relativ top-20 '>
                             {item.Description}
                             </p>
                         </div>
                     </div>
-                    <div className='rounded container w-[60%] justify-between   items-center flex flex-col col-span-1  mx-auto shadow-2xl bg-white'>
-                        <div className='text-4xl font-bold  relative top-32   '> 
+                    <div className='rounded container gap-7 max-xl:w-[100%] w-[60%] h-100 justify-between flex-col  items-center flex  mx-auto shadow-2xl bg-white'>
+                        <div className='text-4xl font-bold max-2xl:mt-10'>
                             <h1>R$ {item.Price} </h1>
                             
                         </div>
-                        <div>
-                            <button type="button" className=" w-80 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-1 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900">Comprar</button>
+                        <div className='w-[70%]'>
+                            <button type="button" className=" w-[100%] focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-1 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900">Comprar</button>
                         </div>
                     
                     </div>
