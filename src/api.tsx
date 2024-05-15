3b4d542e874916c5896557ed65e1aa96c7c3c2aa
Gabriel <gabrildado08@gmail.com>
2024-05-14 21:43:29 -0300

    ADD product search bar

--- src/api.tsx
+++ src/api.tsx
@@ -1,8 +1,8 @@
 
 const ApiBase = 'http://127.0.0.1:8000/api'
 
-interface produxtidprops{
-    id? : string,
+interface produxtidprops {
+    id?: string,
 }
 
 interface commentprops {
@@ -27,31 +27,30 @@ interface dataprops {
 
 
 const API = {
-
     verifylogin: async (email: string, password: string) => {
         const formData = new FormData()
 
         formData.append('email', email)
         formData.append('password', password)
 
-    
-        await fetch(ApiBase + "/verifylogin",{
-            method:'POST',
+
+        await fetch(ApiBase + "/verifylogin", {
+            method: 'POST',
             body: formData
         })
-        .then(response =>{
-            return response.json();
-                })
-            .then(data =>{
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
                 localStorage.removeItem("error")
                 localStorage.removeItem("userid")
                 localStorage.removeItem("userName")
                 let userinfo = data
-                if(userinfo.error != 'error'){
+                if (userinfo.error != 'error') {
                     console.log(userinfo)
                     localStorage.setItem('userid', userinfo.userId)
                     localStorage.setItem('userName', userinfo.firstName)
-                }else{
+                } else {
                     localStorage.removeItem("userid")
                     localStorage.setItem('error', "error")
                     localStorage.removeItem("userName")
@@ -61,211 +60,204 @@ const API = {
     },
 
     userRegister: async (formData: FormData) => {
-
         var success = false
 
-        await fetch(ApiBase + "/userRegister",{
-            method:'POST',
+        await fetch(ApiBase + "/userRegister", {
+            method: 'POST',
             body: formData
         })
-        .then(response =>{
-            return response.json();
-                })
-                .then(data =>{
-                    success = data.success
-                })
-           
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                success = data.success
+            })
+
         return success
     },
 
     addProduct: async (formData: FormData) => {
-
         var success = false
 
-        await fetch(ApiBase + "/saveproduct",{
-            method:'POST',
+        await fetch(ApiBase + "/saveproduct", {
+            method: 'POST',
             body: formData
         })
-        .then(response =>{
-            return response.json();
-                })
-                .then(data =>{
-                    success = data.success
-                    console.log(success)
-                })
-           
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                success = data.success
+                console.log(success)
+            })
+
         return success
     },
 
     editProduct: async (formData: FormData) => {
-
         var success = false
 
-        await fetch(ApiBase + "/editproduct",{
-            method:'POST',
+        await fetch(ApiBase + "/editproduct", {
+            method: 'POST',
             body: formData
         })
-        .then(response =>{
-            return response.json();
-                })
-                .then(data =>{
-                    success = data.success
-                    console.log(success)
-                })
-           
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                success = data.success
+                console.log(success)
+            })
+
         return success
     },
 
-    get_roduct: async (id:string) => {
+    searchProduct: async (name: String) => {
+        return await fetch('http://127.0.0.1:8000/api/getProductByName/' + name)
+    },
 
-        await fetch(ApiBase + "/product/"+id,{
-            method:'GET',
+    get_roduct: async (id: string) => {
+        await fetch(ApiBase + "/product/" + id, {
+            method: 'GET',
         })
-        .then(response =>{
-            return response.json();
-                })
-                .then(data =>{
-                    return data
-                })
-           
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                return data
+            })
+
         return []
     },
 
     saveComment: async (formData: FormData) => {
-
         var success = false
 
-        await fetch(ApiBase + "/saveComment",{
-            method:'POST',
+        await fetch(ApiBase + "/saveComment", {
+            method: 'POST',
             body: formData
         })
-        .then(response =>{
-            return response.json();
-                })
-                .then(data =>{
-                    success = data.success
-                })
-           
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                success = data.success
+            })
+
         return success
     },
     getAllComment: async () => {
-
-        await fetch(ApiBase + "/getAllComment",{
-            method:'GET',
+        await fetch(ApiBase + "/getAllComment", {
+            method: 'GET',
         })
-        .then(response =>{
-            return response.json();
-                })
-                .then((data: commentprops[]) =>{
-                    console.log(data)
-                    return data
-                })
-           
+            .then(response => {
+                return response.json();
+            })
+            .then((data: commentprops[]) => {
+                console.log(data)
+                return data
+            })
+
         return []
     },
 
-    deleteComment: async (id:string) =>{
-
+    deleteComment: async (id: string) => {
         var success = false
 
         const formData = new FormData()
         formData.append('commentid', id)
-        
-        await fetch(ApiBase + "/DeleteComment",{
-            method:'POST',
+
+        await fetch(ApiBase + "/DeleteComment", {
+            method: 'POST',
             body: formData
         })
-        .then(response =>{
-            return response.json();
-                })
-                .then(data =>{
-                    console.log(data)
-                    success = data.success
-                })
-           
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data)
+                success = data.success
+            })
+
         return success
 
     },
 
-    deleteProduct: async (id:string) =>{
-
+    deleteProduct: async (id: string) => {
         var success = false
 
         const formData = new FormData()
         formData.append('productid', id)
-        
-        await fetch(ApiBase + "/DeleteProduct",{
-            method:'POST',
+
+        await fetch(ApiBase + "/DeleteProduct", {
+            method: 'POST',
             body: formData
         })
-        .then(response =>{
-            return response.json();
-                })
-                .then(data =>{
-                    console.log(data)
-                    success = data.success
-                })
-           
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data)
+                success = data.success
+            })
+
         return success
     },
 
-    deleteOrder: async (id:string, id2: string) =>{
-
+    deleteOrder: async (id: string, id2: string) => {
         var success = false
 
         const formData = new FormData()
         formData.append('orderid', id)
         formData.append('orderitemid', id2)
 
-        
-        await fetch(ApiBase + "/Deleteorder",{
-            method:'POST',
+
+        await fetch(ApiBase + "/Deleteorder", {
+            method: 'POST',
             body: formData
         })
-        .then(response =>{
-            return response.json();
-                })
-                .then(data =>{
-                    console.log(data)
-                    success = data.success
-                })
-           
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data)
+                success = data.success
+            })
+
         return success
     },
 
     editComment: async (formData: FormData) => {
-
         var success = false
 
-        await fetch(ApiBase + "/editComment",{
-            method:'POST',
+        await fetch(ApiBase + "/editComment", {
+            method: 'POST',
             body: formData
         })
-        .then(response =>{
-            return response.json();
-                })
-                .then(data =>{
-                    success = data.success
-                })
-           
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                success = data.success
+            })
+
         return success
     },
 
     createOrder: async (formData: FormData) => {
-
         var success
 
-        await fetch(ApiBase + "/createOrder",{
-            method:'POST',
+        await fetch(ApiBase + "/createOrder", {
+            method: 'POST',
             body: formData
         })
-        .then(response =>{
-            return response.json();
-                })
-                .then(data =>{
-                    success = data
-                })
-           
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                success = data
+            })
+
         return success
     },
 
--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,6 +1,7 @@
 import { Slider } from "../../components/slider"
 import { FiShoppingCart } from "react-icons/fi";
 import { useEffect, useState } from "react"
+import API from "../../api";
 import './style.css'
 import { Link } from "react-router-dom";
 
@@ -16,57 +17,84 @@ interface dataprops {
 }
 
 export function Home() {
-
     const [datac, setData] = useState<dataprops[]>([])
+    const [search, setsearch] = useState('')
+
+    function getData() {
+        fetch('http://127.0.0.1:8000/api/allProducts')
+            .then(Response => Response.json())
+            .then((data: dataprops[]) => {
+                setData(data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
 
     useEffect(() => {
+        getData()
+    }, [])
 
-        function getData() {
-            fetch('http://127.0.0.1:8000/api/allProducts')
+    useEffect(() => {
+        if (search !== '') {
+            API.searchProduct(search)
                 .then(Response => Response.json())
                 .then((data: dataprops[]) => {
-                    console.log(data)
                     setData(data)
                 })
                 .catch((err) => {
                     console.log(err)
                 })
-        }
 
-        getData()
-    }, [])
+        } else {
+            getData()
+        }
+    }, [search])
 
     return (
         <main className="">
             <Slider />
             <div className="w-full flex justify-center">
                 <div className="container shadow-2xlrounded w-full max-w-6xl gap-5 mt-[150px] p-10 flex flex-wrap mb-10 justify-evenly bg-white">
-                    {datac.map((item) => (
-                        <div className="flex flex-wrap justify-evenly shadow-2xl w-[270px]">
-                            <div className="flex flex-col  align-baseline h-[370px]  w-[97%] rounded overflow-hidden justify-between mb-">
-                                <Link to={'/product/' + item.ProductID}>
-                                    <div className=" ">
-                                        <img
-                                            className="rounded-t-lg  mx-auto"
-                                            src={item.url}
-                                            width={150}
-                                            alt=""
-                                        />
-                                    </div>
-                                </Link>
-                                <div className="px-6 py-4 info">
-                                    <div className="font-bold text-xl max-w-xl mb-"><p className=" break-words name text-ellipsis">{item.ProductName}</p></div>
-                                    <div className="price flex justify-end  items-center h-[100%] ">
-                                        <p className="text-gray-700 text-base w-[100%]">
-                                            <span className="text-lg text-green-500 font-bold">R$ {item.Price}</span>
-                                        </p>
+                    <div className="relative">
+                        <input
+                            id=""
+                            name="search" type="text"
+                            className={`peer placeholder-transparent h-10 w-full border-b-2 ${localStorage.getItem('error') ? "border-red-400" : "border-gray-400"} border-gray-400 text-gray-900 focus:outline-none focus:borer-rose-600`}
+                            value={search}
+                            onChange={(e) => setsearch(e.target.value)}
+                            placeholder="Pesquisar" />
+                        <label className={`absolute left-0 -top-3.5 ${localStorage.getItem('error') ? "text-red-600" : "text-gray-600"} text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-sm`}>Pesquisar</label>
+                    </div>
+                    <div className="container shadow-2xlrounded w-full max-w-6xl gap-5 mt-[150px] p-10 flex flex-wrap mb-10 justify-evenly bg-white">
+                        {datac.map((item) => (
+                            <div className="flex flex-wrap justify-evenly shadow-2xl w-[270px]">
+                                <div className="flex flex-col  align-baseline h-[370px]  w-[97%] rounded overflow-hidden justify-between mb-">
+                                    <Link to={'/product/' + item.ProductID}>
+                                        <div className=" ">
+                                            <img
+                                                className="rounded-t-lg w-44  h-64 mx-auto"
+                                                src={item.url}
+                                                width={150}
+                                                height={150}
+                                                alt=""
+                                            />
+                                        </div>
+                                    </Link>
+                                    <div className="px-6 py-4 info">
+                                        <div className="font-bold text-xl max-w-xl mb-"><p className=" break-words name text-ellipsis">{item.ProductName}</p></div>
+                                        <div className="price flex justify-end  items-center h-[100%] ">
+                                            <p className="text-gray-700 text-base w-[100%]">
+                                                <span className="text-lg text-green-500 font-bold">R$ {item.Price}</span>
+                                            </p>
 
-                                        <i className="items-end "><FiShoppingCart /></i>
+                                            <i className="items-end "><FiShoppingCart /></i>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
         </main>
